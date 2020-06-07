6ec549adc4444dd753b5f10d85231f2d20573eb2
AllwinWillbert <66547141+AllwinWillbert@users.noreply.github.com>
2020-06-07 22:57:35 +0530

    venduthal.py
    thevaigalai purthi seiya arul puriyum andavare

--- first.py
+++ first.py
@@ -4,3 +4,9 @@ print('thank u parisuthavi')
 
 print('engalayai nilaivalvil setharulum')
 print('amen')
+
+
+print('appa pithave intha corona thakkam athigamai ulla kalathule engalai polapilurunthu katharulum')
+print('melum engalai sothaniyil udpaduthamal themailirunthu kaathu nalla sugathodum asirvathathodum valnthu valara engal matrum ella kudumbagalaiyum asirvathiyum')
+print('kadan adainthu mamtrum nalla velai,veedu kidaika kirubai tharum ayya')
+print('enaku beaulavai um sitham irunthal enodu sernthu omaku etha kudumabamai vala kirubai tharum')
