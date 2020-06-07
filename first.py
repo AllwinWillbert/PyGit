d9d47dcf7ea379819b6b1611b428e47b395eb0b8
AllwinWillbert <66547141+AllwinWillbert@users.noreply.github.com>
2020-06-07 23:02:41 +0530

    space deleted
    unwanted space removed

--- first.py
+++ first.py
@@ -5,7 +5,6 @@ print('thank u parisuthavi')
 print('engalayai nilaivalvil setharulum')
 print('amen')
 
-
 print('appa pithave intha corona thakkam athigamai ulla kalathule engalai polapilurunthu katharulum')
 print('melum engalai sothaniyil udpaduthamal themailirunthu kaathu nalla sugathodum asirvathathodum valnthu valara engal matrum ella kudumbagalaiyum asirvathiyum')
 print('kadan adainthu mamtrum nalla velai,veedu kidaika kirubai tharum ayya')
